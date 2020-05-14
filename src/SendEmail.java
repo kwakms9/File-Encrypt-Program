8e4ae9b7d4b74feaade333e40f2a42bdc796a842
KMS <good6676g@gmail.com>
2020-05-15 00:02:51 +0900

    이메일기능으로 이메일 인증코드전송 구현

---
+++ src/SendEmail.java
@@ -0,0 +1,213 @@
+import javax.mail.AuthenticationFailedException;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.*;
+
+
+public class SendEmail {
+    private String username = null; // null 이면 logout 상태
+    private String password = null;
+    String yesorno;
+	Scanner scan = new Scanner(System.in);
+	boolean success = false;
+	boolean guimode = false;
+    SendEmail(){
+    }
+    
+	
+	
+    public void clearScreen() {
+        for (int i = 0; i < 80; i++)
+          System.out.println("");
+      }
+    
+    public void menu() {
+	        System.out.println("사용하실 옵션을 선택해 주십시오.");
+	        System.out.println("=========================");
+	        if(username==null) { System.out.println("1.로그인"); }
+	        else { System.out.println("1.다른계정으로 로그인"); }
+	        System.out.println("2.메일 작성");
+	        System.out.println("3.로그아웃");
+	        System.out.println("4.종료");
+	        System.out.println("========================="); 
+	        System.out.printf("입력: ");
+	        
+        }
+    public void login() {
+    	do {
+    		username=checkId();
+    		if(guimode==false) {
+		    	System.out.printf("비밀번호를 입력하시오: ");
+		    	password = scan.next();
+		    	clearScreen();	//화면 올리기
+    		}
+	    	tryLogin();
+	    	if(success == true) {
+	    		break;
+	    	}
+    	}while(!guimode);
+    }
+    
+    public String getUsername() {
+		return username;
+	}
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public void logout() {
+    	if(username!=null) {
+    		username = null;
+    		password = null;
+    	}else {
+    		System.out.println("현재 로그아웃 상태입니다.");
+    		System.out.println("");
+    	}
+    }
+    
+    public String checkId() {
+    	String tmp[] = new String[2];
+    	
+    	do {
+    		if(guimode==false) {
+    			System.out.printf("구글 이메일을 입력하시오: ");
+    			username = scan.next();
+    			}
+        	
+        	if(!username.contains("@")) {
+        		username= username+"@gmail.com";
+        	}
+        	tmp = username.split("@");
+    		
+	    	if(tmp[1].equalsIgnoreCase("gmail.com")) {	//포털 주소 확인
+	    		username = tmp[0]+"@gmail.com";
+	    		break;
+	    	}else {
+	    		System.out.println("사용할 수 없는 이메일주소입니다.");
+	    		System.out.println("입력된 포털주소: "+tmp[1]);
+	    	}
+    	}while(!guimode);
+    	return username;
+    }
+    public void tryLogin() {
+    	String receiver;
+    	receiver = "test@lagsixtome.com";	//없는 이메일
+    	sender(receiver,"","");
+    	if(success) {
+    		System.out.println("로그인 성공");
+    	}
+    	
+    }
+    
+    public void sender(String receiver, String title, String content) {
+    	try{
+    		
+            Properties prop = new Properties();
+            prop.put("mail.smtp.host", "smtp.gmail.com");
+            prop.put("mail.smtp.port", "587");
+            prop.put("mail.smtp.auth", "true");
+            prop.put("mail.smtp.starttls.enable", "true"); //TLS
+
+            Session session = Session.getInstance(prop,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("NotUsed@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.BCC,
+                    InternetAddress.parse(receiver)
+            );
+            message.setSubject(title);
+
+            message.setContent(content, "text/html;charset=UTF-8");
+
+            Transport.send(message);
+
+            //System.out.println("Email sent.");
+            success = true;
+
+        } catch (AuthenticationFailedException e) {
+        	System.out.println("로그인 실패!\n아이디와 비밀번호를 다시 확인해 보십시오.");
+        	success = false;
+            //e.printStackTrace();
+        } catch(Exception e) {
+        	System.out.println("메일 전송중 에러 발생!");
+        	success = false;
+        }
+    }
+
+    public void writeMessage() {
+    	String title;
+    	String content;
+    	
+    	if(username!=null) {
+	    	while(true) {
+		    	System.out.printf("보낼사람의 이메일 주소를 입력하십시오.\n: ");
+		    	scan.nextLine();
+		    	String receiver [] = scan.nextLine().replace(" ", "").split(","); //이메일 주소들 입력
+		    	
+		    	System.out.printf("메일의 제목을 입력하십시오.\n: ");
+		    	title = scan.nextLine();
+		    	System.out.println("내용을 입력하시오.\n: ");
+				content = scan.nextLine();
+				System.out.println("전송하시겠습니까?(Y:전송 N:취소):");
+				yesorno = scan.next();
+				if(yesorno.equalsIgnoreCase("Y") || yesorno.contentEquals("전송")) {
+					System.out.println("메일 전송중...");
+					for(int i=0;i<receiver.length;i++) {
+						System.out.println(receiver[i]);
+						sender(receiver[i],title,content);
+					}
+					System.out.println("모든 메일을 발송하였습니다. 더 보내시겠습니까?(Y:전송 N:돌아가기)");
+					yesorno = scan.next();
+					if(!(yesorno.equalsIgnoreCase("Y") || yesorno.contentEquals("전송"))) { break; }
+					
+				}else {
+					System.out.println("메일 전송이 중단되었습니다. 처음으로 이동");
+				}
+	    	}
+    	}else {
+    		System.out.println("현재 로그인 상태가 아닙니다.");
+    		System.out.println("");
+    	}
+    }
+    
+    public String guiMessage(String receiverarry, String title, String content) {
+    	String errreceiver="";
+    	String receiver [] = receiverarry.replace(" ", "").split(","); //이메일 주소들 입력
+		System.out.println("메일 전송중...");
+		
+		for(int i=0;i<receiver.length;i++) {
+			sender(receiver[i],title,content);	//전송
+			if(success==false) {
+				errreceiver+=receiver[i]+" ";
+			}
+		}
+	    	return errreceiver;
+    }
+}
--- src/UserLogin.java
+++ src/UserLogin.java
@@ -13,30 +13,199 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Random;
 
 public class UserLogin {
 	private String username = null; // null 이면 logout 상태
 	private String password = null;
-
-	String savePath = System.getProperty("user.home") + "\\Documents\\FileEncrytProgram";//정보저장 경로
-
-	//File file = new File(savePath);
-	
+	private String savePath = System.getProperty("user.home") + "\\Documents\\FileEncrytProgram";//정보저장 경로
 	BufferedReader in = null;
-	PrintWriter out = null;
+	PrintWriter out = null;//out = new PrintWriter(new BufferedWriter(new FileWriter(savePath+"\\data.bin")));//true없으므로 덮어쓰기
+	String userData[][];
+	String tmpUserData = "";
 	
-	UserLogin() {
+	UserLogin() {	//로그인을 위한 파일 불러오기
 		
 		File Folder = new File(savePath);
-		
+		File data = new File(savePath+"\\data.txt");
 		if (!Folder.exists()) {
 			try {
 				Folder.mkdir();	//폴더가 없을 경우 생성
+				data.createNewFile(); // 파일 생성
 			} catch (Exception e) {
 				e.getStackTrace();
 			}
+		}else if(!data.exists()) {//폴더있지만 파일 없을시
+			try {
+				data.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
-
+		fileLoad();
+		
 	}
+	
+	
+	public void fileLoad() {
+		try {
+			in = new BufferedReader(new FileReader(savePath+"\\data.txt"));
+			
+			String temp;
+			while((temp=in.readLine()) != null) { tmpUserData+=temp+"\n"; }	//사용자 정보 읽기
+			//System.out.println(userData);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		if(!tmpUserData.equals("")) {//파일이 비었는지 확인
+			String splitTmp[] =tmpUserData.split("\n");
+			userData = new String[splitTmp.length][];
+			
+			for(int i=0;i<splitTmp.length;i++) {
+				userData[i] = splitTmp[i].split("##");//아이디 비번으로 분리  0 id, 1 pw, 2 email
+			}
+		}
+	}
+	
+	
+	public void login(String username, String password) {
+		this.username = username;
+		//this.password = password;//gui전임시
+		
+		if(idOrPwCheck(username,password)) {
+			System.out.println("로그인 성공");
+		}else {
+			System.out.println("실패");
+		}
+	}
+	
+	
+	public boolean signUp(String id,String pw) {//가입시 중복체크밑 정보입력 
+		if(!idOrPwCheck(id)) {	//중복이 아닐경우
+			//this.username=id;
+			//this.password=pw;
+			savefile(id+"##"+pw);
+			fileLoad();	//reload file
+			return true;
+		}else {
+			System.out.println("중복된 ID");
+		}
+		return false;
+	}
+	
+	public boolean signUp(String id,String pw,String email) {//가입시 중복체크밑 정보입력  @이메일에 따라 오버로딩
+		if(!idOrPwCheck(id)) {	//중복이 아닐경우
+			//this.username=id;
+			//this.password=pw;
+			savefile(id+"##"+pw+"##"+email);
+			fileLoad();	//reload file
+			return true;
+		}else {
+			System.out.println("중복된 ID");
+		}
+		return false;
+	}
+	
+	
+	public void savefile(String str) {
+		try {
+			out = new PrintWriter(new BufferedWriter(new FileWriter(savePath+"\\data.txt",true)));//true=없으므로 덮어쓰기, true면 append
+			out.write(str+"\n");	//줄넘김 없으니까 추가
+			out.flush();
+			out.close();
+			}catch(IOException e) { e.printStackTrace(); }
+	}
+	
+	
+	public boolean idOrPwCheck(String username) {	//아이디만 있는지 확인
+		for(int i=0;i<userData.length;i++) {
+			if(userData[i][0].equals(username)) {
+				System.out.println("사용불가능한 ID");
+				return true;	//있다
+				}
+		}
+		return false;	//못찾았을경우 중복되지 않는 아이디
+	}
+	
+	public boolean idOrPwCheck(String username, String password) {	//아이 비번 확인
+		for(int i=0;i<userData.length;i++) {
+			if(userData[i][0].equals(username)) {
+				if(userData[i][1].equals(password)) {
+					return true;
+				}
+			}
+		}
+		return false;	//못찾았을경우
+	}
+	
+	public String checkEmail(String email) {//얘도 findId와 합치기
+		for(int i=0;i<userData.length;i++) {
+			if(userData[i].length == 3) {	//이메일 있는게 길이가 3
+				if(email.equals(userData[i][2])) {//이메일 일치 여부 확인
+					return userData[i][0];
+				}
+			}
+		}
+		return null;	//불일치
+	}
+	
+	public String findEmail(String id) {//findPw메소드와 합치기
+		String tmp="";
+		for(int i=0;i<userData.length;i++) {
+			if(userData[i].length == 3) {	//이메일 있는게 길이가 3
+				if(id.equals(userData[i][0])) {//이메일 일치 여부 확인
+					tmp+= userData[i][2]+" ";	//한개의 이메일로 2개 이상 가입했을수도있으므로
+				}
+			}
+		}
+		if(!tmp.equals(""))	
+		{
+			return tmp;	// 아이디들 보내기
+		}
+		return null;	//불일치
+	}
+	
+	public void findId(String email) {	//이메일이 등록된 경우만 가능
+		String id = checkEmail(email);
+		if(id !=null) {//이메일 일치 여부 확인
+			SendEmail mail = new SendEmail();
+			mail.sender(email, "File-Encrypt-Program - your ID", "당신의 아이디는 "+id+" 입니다");
+			System.out.println("당신의 이메일로 아이디가 전송되었습니다.");
+			return;
+		}else {
+		System.out.println("해당 이메일로 가입된 계정이 없습니다.");
+		}
+	}
+	
+	public String sendVerificationCode(String email) {	//찾을 id 입력받은후
+		System.out.println("이메일로 인증코드발송");
+		SendEmail mail = new SendEmail();
+		String verificationCode="";
+		
+		for(int j=0;j<6;j++) {
+		verificationCode+=""+(int)Math.random()*10;
+		}
+		
+		mail.sender(email, "File-Encrypt-Program", "인증코드: "+verificationCode);
+		System.out.println("당신의 이메일로 인증코드가 전송되었습니다.");
+		return verificationCode;
+	}
+	
+	public void findPw(String id) {
+		String email = findEmail(id);
+		String code;
+		if(email != null ) {//이메일 일치 여부 확인
+			code=sendVerificationCode(email);
+		}else {
+			System.out.println("해당 아이디에 등록된 이메일이 없습니다.");
+		}
+	}
+	
 }
